b55d0326b2b61c0536a0cc4ad9d3926649b0535f
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-15 17:50:24 +0900

    added

--- example/main.go
+++ example/main.go
@@ -1,7 +1,50 @@
 package main
 
-import memory_retention "github.com/yuto51942/memory-retention"
+import (
+	"fmt"
+	"net/http"
+
+	memory_retention "github.com/yuto51942/memory-retention"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	ip := r.RemoteAddr
+	result, err := memory_retention.CreateKey(ip)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	fmt.Fprintln(w, result)
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Query().Get("key")
+
+	if err := memory_retention.AddAnswer(key, "hoge"); err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+}
+
+func getAllHandler(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Query().Get("key")
+	result, err := memory_retention.GetAnswer(key)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	fmt.Fprintln(w, result)
+}
 
 func main() {
-	memory_retention.Hoge()
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/create", createHandler)
+	http.HandleFunc("/add", addHandler)
+	http.HandleFunc("/get", getAllHandler)
+
+	http.ListenAndServe(":8080", nil)
 }
--- memory_retention.go
+++ memory_retention.go
@@ -1,7 +1,128 @@
 package memory_retention
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+)
 
-func Hoge() {
-	fmt.Println("OK")
+var mutex = &sync.Mutex{}
+
+// This global variable used save client answers.
+// - Key is a hash value that identifies an individual.
+var data = make(map[string][]string)
+
+// Save whether the key is in use.
+// If the key does not exist in this map, it is not used.
+var keyMap = make(map[string]bool)
+
+// Create a key.
+//
+// Arguments:
+// seed {string} - Seed value for generating the key.
+//
+// Returns:
+// {string} - The generated hash value. Use this value to add/remove values.
+func CreateKey(seed string) (string, error) {
+	var strBuild strings.Builder
+
+	strBuild.WriteString(seed)
+	strBuild.WriteString(time.Now().String())
+
+	result := sha256.Sum256([]byte(strBuild.String()))
+	hash := hex.EncodeToString(result[:])
+
+	mutex.Lock()
+	keyMap[hash] = true
+	mutex.Unlock()
+
+	return hash, nil
+}
+
+// Deletes the specified key.
+//
+// Arguments:
+// key {string} - Key value.
+func DeleteKey(key string) error {
+	mutex.Lock()
+	if err := exist(key); err != nil {
+		return err
+	}
+
+	delete(keyMap, key)
+	delete(data, key)
+	mutex.Unlock()
+	return nil
+}
+
+// Clear the answer.
+//
+// Arguments:
+// key {string} - Key value.
+func DeleteAnswer(key string) error {
+	mutex.Lock()
+	if err := exist(key); err != nil {
+		return err
+	}
+	data[key] = []string{}
+	mutex.Unlock()
+	return nil
+}
+
+// All clear.
+func DeleteAll() {
+	data = make(map[string][]string)
+	keyMap = make(map[string]bool)
+}
+
+// Check if the key exists.
+// If the key does not exist, an error will be thrown.
+//
+// Arguments:
+// key {string} - Key value.
+func exist(key string) error {
+	_, exist := keyMap[key]
+	if !exist {
+		return fmt.Errorf("Key is not exist.")
+	}
+	return nil
+}
+
+// Add an answer.
+//
+// Arguments:
+// key {string} - Key value.
+// value {string} - Answer.
+func AddAnswer(key string, value string) error {
+	mutex.Lock()
+	if err := exist(key); err != nil {
+		return err
+	}
+	if data[key] == nil {
+
+	}
+	data[key] = append(data[key], value)
+	mutex.Unlock()
+	return nil
+}
+
+// Returns a list of answers.
+//
+// Arguments:
+// key {string} - Key value.
+//
+// Returns:
+// {[]string} - Answers.
+func GetAnswer(key string) ([]string, error) {
+	mutex.Lock()
+	if err := exist(key); err != nil {
+		return nil, err
+	}
+	answer := data[key]
+	mutex.Unlock()
+
+	return answer, nil
 }
